726f44ec24688a2981134a7b68eddb8b19600f02
Edgar <edgrr.trem000@gmail.com>
2019-08-15 11:30:35 +0700

    add ios example

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,10 +1,16 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+// import the plugin
+// #import "FlutterConfigPlugin.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // then read individual keys like:
+     // NSString *apiUrl = [FlutterConfigPlugin envFor:@"API_URL"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
---
+++ ios/Classes/GeneratedDotEnv.m
@@ -0,0 +1,1 @@
+  #define DOT_ENV @{ @"APP_NAME":@"Flutter Config Example",@"FABRIC_ID":@"someId555" };
