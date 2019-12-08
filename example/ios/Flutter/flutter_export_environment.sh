345179837225fc4993a8b7264006f549b7babff9
Edgar <edgrr.trem000@gmail.com>
2019-12-08 18:00:38 +0700

    setting up multiple schemes on ios

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,8 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/edgar/Development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/edgar/Development/Flutter-Projects/flutter_config/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/edgar/Development/Flutter-Projects/flutter_config/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/edgar/Development/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
